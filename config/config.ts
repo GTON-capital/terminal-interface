ee902a51d0c887eb358c1353d500765e1d938f33
GooGrand <55867268+GooGrand@users.noreply.github.com>
2022-01-09 07:15:45 +0600

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -12,9 +12,9 @@ const isDev = process.env.NODE_ENV === 'development';
 const isLive = 'true';
 const backednAddress = 'https://mining.gearbox-api.com';
 
-const stakingAddress = '0xAc87D270029A4d55a3eb2f9e8b2846a026A8a72e';
-const faucetAddress = '0xB85D194500b177516Fb252c92851158DB81b305F';
-const tokenAddress = '0xEb172F12C9D5E08d0A551050e33464138039aA9E';
+const stakingAddress = '0x722A280a0178c500A1bCCe3a9e519a6b669bf912';
+const faucetAddress = '0x013Da22e5aa1Aa49cae61A1b06B240164021CF22';
+const tokenAddress = '0xc4d0a76BA5909c8e764B67aCF7360f843FbACB2d';
 const fantomTestnet = {
   chainId: '4002',
   chainIdHex: '0xFA2',
