d5397907b43cced3c52b2b7e9a7bb6e28c336b6e
GooGrand <sharkanovnikita@gmail.com>
2022-01-18 01:52:54 +0600

    changed preview

--- components/SiteHead/SiteHead.tsx
+++ components/SiteHead/SiteHead.tsx
@@ -15,7 +15,7 @@ function SiteHead({
   keyWords,
   url,
   favIcon = '/images/favicon.svg',
-  image = '/images/card.jpeg',
+  image = '/images/preview.png',
 }: ISiteHeadProps) {
   return (
     <Head>
