46e6432ebad4b92e6cadf5ec00449bc0ba518bb4
GooGrand <sharkanovnikita@gmail.com>
2022-02-04 18:57:25 +0600

    Fix: add trim for args

--- Parser/GTONCapitalProjects/GTONCapitalRouter.tsx
+++ Parser/GTONCapitalProjects/GTONCapitalRouter.tsx
@@ -271,7 +271,7 @@ async function Parse(eventQueue, command)
 {
   const { print } = eventQueue.handlers;
   const Command = command.split(' ')[0].trim().toLowerCase();
-  const Arg = command.split(' ')[1];
+  const Arg = command.split(' ')[1].trim().toLowerCase();
 
   try
   {
