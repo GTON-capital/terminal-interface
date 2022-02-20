a986db6b3e3347b74e6e30825f6dc919d47300f4
RND332 <36820511+RND332@users.noreply.github.com>
2022-02-20 15:27:47 +0300

    Testnet update
    Changing the SDK function to ethers, the necessary data for calculating commissions has been added to the config, rounding up for $FTM, in order to be sure that the user will receive at least the specified amount

--- Parser/WEB3/buyGTON.ts
+++ Parser/WEB3/buyGTON.ts
@@ -1,8 +1,5 @@
 import { ethers } from 'ethers';
-import {
-  SpiritSwapRouterAddress,
-  FTMGTONSwapPath as path,
-} from '../../config/config';
+import { FTMGTONSwapPath as path, spiritswaprouteraddress } from '../../config/config';
 import SpiritSwapRouterABI from './ABI/SpiritSwapRouter.json';
 import { validate } from './validate';
 
@@ -10,23 +7,27 @@ import { validate } from './validate';
 
 declare const window: any;
 
-const buy = async (amount, gtonftmprice): Promise<string> => {
+const buy = async (amount, gtonftmprice): Promise<string> => 
+{
   await validate();
-
+  
   const CurrentUnixTime = Math.round((new Date()).getTime() / 1000);
   const Deadline = CurrentUnixTime + 1200; // Current time + 20 minutes
 
   const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
   const signer = provider.getSigner(0);
-  const contract = new ethers.Contract(SpiritSwapRouterAddress, SpiritSwapRouterABI, signer);
+  const contract = new ethers.Contract(spiritswaprouteraddress, SpiritSwapRouterABI, signer);
 
-  gtonftmprice =  ethers.utils.parseUnits(gtonftmprice.toString(), 18)
-  amount =        ethers.utils.parseUnits(amount.toString(), 18)
-  
+  // Rounding up to be sure that the transaction will pass
+  gtonftmprice = Math.ceil(gtonftmprice * 100)/100;
+
+  gtonftmprice =  ethers.utils.parseUnits(gtonftmprice.toString(), 18);
+  amount =        ethers.utils.parseUnits(amount.toString(), 18);
+
   const tx = await contract.swapExactETHForTokens(amount, 
                                                 path, 
                                                 signer.getAddress(), 
-                                                Deadline, { value: gtonftmprice.toString(), })
+                                                Deadline, { value: gtonftmprice, })
   console.log(tx)
   const receipt = await tx.wait();
   return receipt.transactionHash;
--- config/config-test.ts
+++ config/config-test.ts
@@ -1,11 +1,15 @@
-
 const network = '4002';
 const isDev = process.env.NODE_ENV === 'development';
 const isLive = 'true';
 const isTestnet = true;
 
-const spiritswaprouteraddress = '0x406b838e5ac09d90e7cb48187ad7f4075184eb28';
-const FTMGTONSwapPath = ['0x39575ceC00dBa1408Baf761A3e093322A6Ce3b94', '0xc4d0a76ba5909c8e764b67acf7360f843fbacb2d']
+const WFTMAddress = "0x684ef2e18b9e1aefeeaf82bef1cfe37f3f07f162";
+const GTONAddress = "0xc4d0a76BA5909c8e764B67aCF7360f843FbACB2d";
+
+const spiritswaprouteraddress = '0x5B1769426C7AAf156d44A7fd542394B3b26dD134';
+const spiritswappooladdress = '0x5B1769426C7AAf156d44A7fd542394B3b26dD134';
+const FTMGTONSwapPath = ['0x684ef2e18b9e1aefeeaf82bef1cfe37f3f07f162', '0xc4d0a76BA5909c8e764B67aCF7360f843FbACB2d']
+
 const stakingAddress = '0x314650ac2876c6B6f354499362Df8B4DC95E4750';
 const faucetAddress = '0x013Da22e5aa1Aa49cae61A1b06B240164021CF22';
 const tokenAddress = '0xc4d0a76BA5909c8e764B67aCF7360f843FbACB2d';
@@ -13,7 +17,7 @@ const fantomNet = {
   chainId: '4002',
   chainIdHex: '0xFA2',
   chainName: 'Fantom Testnet',
-  rpcUrls: ['https://rpc.testnet.fantom.network'],
+  rpcUrls: ['https://rpc.testnet.fantom.network/'],
   nativeCurrency: { name: 'FTM', decimals: 18, symbol: 'FTM' },
   blockExplorerUrls: ['https://testnet.ftmscan.com/'],
 };
@@ -21,6 +25,9 @@ const ftmscanUrl = "https://testnet.ftmscan.com/tx/"
 const faucetLink = "https://faucet.fantom.network/"
 const gcLink = "https://gton.capital/"
 export {
+  WFTMAddress,
+  GTONAddress,
+  spiritswappooladdress,
   spiritswaprouteraddress,
   FTMGTONSwapPath,
   faucetLink,
