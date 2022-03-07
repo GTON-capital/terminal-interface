8644e092858337748be76fe3991cd51266b8e32c
GooGrand <sharkanovnikita@gmail.com>
2022-03-07 23:47:12 +0600

    Upd: add message printer

--- Parser/Chat/Parser.ts
+++ Parser/Chat/Parser.ts
@@ -23,18 +23,30 @@ const sendWorker = createWorker(async ({ print }, args) => {
     print([textLine({ words: [textWord({ characters: "Available bond types: " })] })]);
 })
 
-const loginWorker = createWorker(async ({ print }, args) => {
-    // TODO check if user is already logged in
-
+const loginWorker = createWorker(async ({ print }, args, [userAddress]) => {
+    const addresses = await getWhitelist();
+    if(userAddress in addresses) {
+        throw new Error();
+    }
     const name = (args.split(" ")[0]).trim();
     const openKey = await getOpenKey()
     const sign = await signData(openKey)
-    const data = await makeRequest(Routes.Login, { open_key: openKey, sign: sign.substring(2), name })
+    await makeRequest(Routes.Login, { open_key: openKey, sign: sign.substring(2), name })
     print([textLine({ words: [textWord({ characters: "Available bond types: " })] })]);
 }, "Something went wrong while registering")
 
 const loadWorker = createWorker(async ({ print }, args, [userAddress]) => {
-    print([textLine({ words: [textWord({ characters: "Available bond types: " })] })]);
+    const limit = args;
+    const res = await makeRequest(Routes.Get, { address: userAddress, limit })
+    print([textLine({ words: [textWord({ characters: `Last ${limit} messages:` })] })]);
+    res.forEach((val) => {
+        // 2022-02-24T12:25:46.362826
+        const date = val.ts.split(/[T.]*$/)
+        print([textLine({ words: [textWord({ characters: `Date - ${date[0]} ${date[1]}` })] })]);
+        print([textLine({ words: [textWord({ characters: `Sender -  ${val.name} (Address - ${val.from_address})` })] })]);
+        print([textLine({ words: [textWord({ characters: `Message -   ${val.paylaod}` })] })]);
+        print([textLine({ words: [textWord({ characters: "--------------------------------------------" })] })]);
+    })
 })
 
 const membersWorker = createWorker(async ({ print }) => {
