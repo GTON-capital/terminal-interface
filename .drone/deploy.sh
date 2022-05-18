a9215a478f9fbe2c954a77ee02e2ad5a1af93878
PoulWalcker <pashasimanko123@gmail.com>
2022-05-18 20:03:49 +0400

    remove docker build

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -5,8 +5,8 @@ set -e
 git clean -fd
 git reset --hard
 git pull
-# export NVM_DIR=~/.nvm
-# source ~/.nvm/nvm.sh
-# npm i
-docker build -t cli .
-docker run -d -t 6001:3000 --name cli cli
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+npm i
+npm run build
+npm run generate
