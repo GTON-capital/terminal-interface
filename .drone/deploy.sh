4d7bb56d00103ff6d56b25c8cdf5708e2ced06b8
PoulWalcker <pashasimanko123@gmail.com>
2022-05-18 20:02:06 +0400

    hide npm i

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,19 +2,11 @@
 
 set -e
 
-# git clean -fd
-# git reset --hard
-# git pull
-# export NVM_DIR=~/.nvm
-# source ~/.nvm/nvm.sh
-# npm i
-# npm run dev
-
 git clean -fd
 git reset --hard
 git pull
-export NVM_DIR=~/.nvm
-source ~/.nvm/nvm.sh
-npm i
+# export NVM_DIR=~/.nvm
+# source ~/.nvm/nvm.sh
+# npm i
 docker build -t cli .
 docker run -d -t 6001:3000 --name cli cli
