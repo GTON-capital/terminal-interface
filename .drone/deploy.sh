84fe068425e606c1dea010e6a1f84184a4506499
Poul GC <67789432+PoulWalcker@users.noreply.github.com>
2022-05-18 20:16:51 +0400

    Update deploy.sh

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -5,8 +5,8 @@ set -e
 git clean -fd
 git reset --hard
 git pull
-export NVM_DIR=~/.nvm
-source ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 npm i
 npm run build
 npm run generate
