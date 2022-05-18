1316f505ea2cccada6ee9754bcc87eee76a6b720
PoulWalcker <pashasimanko123@gmail.com>
2022-05-18 20:00:13 +0400

    added npm env

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+# git clean -fd
+# git reset --hard
+# git pull
+# export NVM_DIR=~/.nvm
+# source ~/.nvm/nvm.sh
+# npm i
+# npm run dev
+
+
 git clean -fd
 git reset --hard
 git pull
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 npm i
-npm run dev
+docker build -t cli .
+docker run -d -t 6001:3000 --name cli cli
