98e5ccdb542f59f714646fce361b9429e1049418
PoulWalcker <pashasimanko123@gmail.com>
2022-05-18 19:53:02 +0400

    added npm env

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -5,7 +5,7 @@ set -e
 git clean -fd
 git reset --hard
 git pull
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
 npm i
 npm run dev
